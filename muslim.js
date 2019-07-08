f3c7c509d95b55f04253cce9eb6b7671077ef13c
shayanekat <shaykatfsx@gmail.com>
2019-07-08 11:43:12 +0300

    almost finished files

--- muslim.js
+++ muslim.js
@@ -1,36 +1,38 @@
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
-    let loc = document.querySelector('.coord');
-    let deg = document.querySelector('.quibla');
-    let cite = document.querySelector('.city');
-    let s = document.querySelector('.sobh');
-    let d = document.querySelector('.dhuhr');
-    let a = document.querySelector('.asr');
-    let m = document.querySelector('.maghrib');
-    let i = document.querySelector('.isha');
-    let city = ville.value;
+function myFunction(){
+    window.addEventListener('load', ()=> {
+        let long;
+        let lat;
+        let loc = document.querySelector('.coord');
+        let deg = document.querySelector('.quibla');
+        let cite = document.querySelector('.city');
+        let s = document.querySelector('.sobh');
+        let d = document.querySelector('.dhuhr');
+        let a = document.querySelector('.asr');
+        let m = document.querySelector('.maghrib');
+        let i = document.querySelector('.isha');
+        var city=document.getElementById("ville").value;
 
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position=>{
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(position=>{
+                long = position.coords.longitude;
+                lat = position.coords.latitude;
 
-            var api = "https://muslimsalat.com/"+city+"/daily.json?key=API_KEY&jsoncallback=";
+                var api = "https://muslimsalat.com/"+city+"/daily.json?key=API_KEY&jsoncallback=";
 
-            fetch(api)
-                .then(resp => {
-                    return resp.json();
-                })
-                .then(data =>{
-                    console.log(data);
-                    const {temperature,summary,icon} = data.currently;
-                    tempdeg.textContent = Math.round(((temperature-32)*(5/9))*100)/100;
-                    tempdescr.textContent = summary;
-                    loctzone.textContent = data.timezone;
+                fetch(api)
+                    .then(resp => {
+                        return resp.json();
+                    })
+                    .then(data =>{
+                        console.log(data);
+                        const {shurooq,dhuhr,asr,maghrib,isha} = data.items[0];
+                        loc.textContent = data.latitude+","+data.longitude
+                        deg.textContent = data.qibla_direction
+                        loctzone.textContent = data.timezone;
 
-                    seticon(icon,document.querySelector(".icon"))
-                });
-        })
-    }
-})
+                        seticon(icon,document.querySelector(".icon"))
+                    });
+            })
+        }
+    })
+}
