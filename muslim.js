b90dd50a01036c1df6e8c599e639e2056b4deaad
shayanekat <shaykatfsx@gmail.com>
2019-07-08 18:01:52 +0300

    client-side done

--- muslim.js
+++ muslim.js
@@ -1,5 +1,9 @@
+
 function myFunction(){
-    window.addEventListener('load', ()=> {
+    
+}
+
+    window.addEventListener('click', ()=> {
         let long;
         let lat;
         let loc = document.querySelector('.coord');
@@ -10,31 +14,24 @@ function myFunction(){
         let a = document.querySelector('.asr');
         let m = document.querySelector('.maghrib');
         let i = document.querySelector('.isha');
-        var city=document.getElementById("ville").value;
-
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(position=>{
-                long = position.coords.longitude;
-                lat = position.coords.latitude;
-
-                var api = "https://muslimsalat.com/"+city+"/daily.json?key=API_KEY&jsoncallback=";
-
-                fetch(api)
-                    .then(resp => {
-                        return resp.json();
-                    })
-                    .then(data =>{
-                        console.log(data);
-                        const {shurooq,dhuhr,asr,maghrib,isha} = data.items[0];
-                        loc.textContent = "coordinates : "+data.latitude+","+data.longitude;
-                        deg.textContent = "quibla direction : "+data.qibla_direction;
-                        s.textContent = "sobh : "+shurooq;
-                        d.textContent = "dhuhr : "+dhuhr;
-                        a.textContent = "asr : "+asr;
-                        m.textContent = "maghrib : "+maghrib;
-                        i.textContent = "isha : "+isha;
+        var city = document.getElementById("ville").value;
+        var api = "https://muslimsalat.com/"+city+"/daily.json?key=API_KEY&jsoncallback=";
+        
+            fetch(api)
+                .then(resp => {
+                    return resp.json();
+                })
+                .then(data =>{
+                    console.log(data);
+                    const {shurooq,dhuhr,asr,maghrib,isha} = data.items[0];
+                    loc.textContent = "coordinates : "+data.latitude+","+data.longitude;
+                    deg.textContent = "quibla direction : "+data.qibla_direction;
+                    s.textContent = "sobh : "+shurooq;
+                    d.textContent = "dhuhr : "+dhuhr;
+                    a.textContent = "asr : "+asr;
+                    m.textContent = "maghrib : "+maghrib;
+                    i.textContent = "isha : "+isha;
                     });
             })
-        }
-    })
-}
+
+        
